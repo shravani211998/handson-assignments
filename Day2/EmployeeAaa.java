46aff75468d80ab0d87ce55e32a471d5884896d6
shravani211998 <71273966+shravani211998@users.noreply.github.com>
2020-09-16 17:10:59 +0530

    Update EmployeeAaa.java

--- Day2/EmployeeAaa.java
+++ Day2/EmployeeAaa.java
@@ -2,22 +2,26 @@ class EmployeeAaa{
 
     public static void main(String[] args){
         Employee e[] = new Employee[5];
+        Address ad1 = new Address("maharashtra","nagpur",123);
+        Address ad2 = new Address("tamilNadu","dfg",456);
 
 
 
-        e[0] = new Employee(1,"xyz",6000); 
+        e[0] = new Employee(1,"xyz",6000,ad1); 
         e[1] = new Employee(2,"pqr",2000);
         e[2] = new Employee(3,"ghj",5000);
         e[3] = new Employee(4,"asd",4000);
-        e[4] = new Employee(5,"bnm",8000);
+        e[4] = new Employee(5,"bnm",8000,ad2);
 
-        for(int i=0 ; i<e.length ; i++){
-            if(e[i].salary > 5000){
-             e[i].display();
+        for(Employee ep : e){
+            if(ep.salary > 5000){
+             ep.display();
             }
 
 
-        }   
+        }  
+
+        
     }
 
 
