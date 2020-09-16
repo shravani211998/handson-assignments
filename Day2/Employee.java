45d53d61ce6a37d7f03245a7a543ab328e0bbc3d
shravani211998 <71273966+shravani211998@users.noreply.github.com>
2020-09-16 17:10:57 +0530

    Update Employee.java

--- Day2/Employee.java
+++ Day2/Employee.java
@@ -2,6 +2,7 @@ class Employee{
     int id ; 
     String name;
     double salary;
+    Address ad;
     
     Employee(int id, String name, double salary){
         this.id = id;
@@ -11,9 +12,20 @@ class Employee{
 
     }
 
+    Employee(int id, String name, double salary, Address ad){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.ad = ad;
+
+    }
+
+   
     void display(){
 
-        System.out.println("Id = "+this.id+", Name = "+this.name+", Salary = "+this.salary);
+        System.out.println("Id = "+this.id+", Name = "+this.name+", Salary = "+this.salary+"Address = "+ad);
+
+
 
     }
 
