93174ba46be9bb46816f3c0270902d5e353b3a01
shravani211998 <71273966+shravani211998@users.noreply.github.com>
2020-09-18 14:25:42 +0530

    Create Programmer.java

---
+++ Day4/Programmer.java
@@ -0,0 +1,25 @@
+public class Programmer extends Employee{
+    double salary;
+    public Programmer(String name, String gender, String dob, int phoneNo, Address address, int emId, double salary){
+        super(name,gender,dob,phoneNo,address,emId,salary);
+
+        
+    }
+
+
+    public void testDisplay(){
+
+        super.testDisplay();
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
