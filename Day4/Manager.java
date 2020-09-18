6831875559473c15c7ad6d4c36c7d55fa0f83254
shravani211998 <71273966+shravani211998@users.noreply.github.com>
2020-09-18 14:25:34 +0530

    Create Manager.java

---
+++ Day4/Manager.java
@@ -0,0 +1,34 @@
+public class Manager extends Employee{
+
+
+
+    public Manager(String name, String gender, String dob, int phoneNo, Address address, int emId,double salary){
+        super(name,gender,dob,phoneNo,address,emId,salary);
+        
+        
+
+
+
+
+    }
+
+   
+
+    public void testDisplay(){
+
+        super.testDisplay();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
