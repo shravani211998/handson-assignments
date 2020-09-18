96a9d99ae8c3f9cff1eec9f0dffb88d6de8ec0ac
shravani211998 <71273966+shravani211998@users.noreply.github.com>
2020-09-18 14:25:12 +0530

    Create Admin.java

---
+++ Day4/Admin.java
@@ -0,0 +1,19 @@
+public class Admin extends Employee{
+
+    public Admin(String name, String gender, String dob, int phoneNo, Address address, int emId, double salary){
+        super(name,gender,dob,phoneNo,address,emId,salary);
+    }
+
+
+    public void testDisplay(){
+
+        super.testDisplay();
+        
+
+
+    }
+
+
+
+
+}
